4de7d8af9718874f257061bbafde410411c266c0
AAKASH KUMAR <aakashakay222@gmail.com>
2024-08-22 22:51:39 +0530

    Updating Prototype pattern

--- src/PrototypePattern/car.java
+++ src/PrototypePattern/car.java
@@ -19,16 +19,17 @@ public class car implements Prototype {
 
 
 
-    car(String name,String company,String Model_year ){
+    car(String name,String company,String Model_year,Integer id ){
         this.name=name;
         this.company=company;
         this.Model_year=Model_year;
+        this.id=id;
 
     }
 
 
     @Override
     public Prototype makeclone() {
-        return new car(name,company,Model_year);
+        return new car(name,company,Model_year,id);
     }
 }
