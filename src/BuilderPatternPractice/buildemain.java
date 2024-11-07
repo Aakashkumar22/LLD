c620e5423ae4f39012aa055fc1a415fce6cffe16
AAKASH KUMAR <aakashakay222@gmail.com>
2024-11-07 11:56:14 +0530

    builder practice

--- src/BuilderPatternPractice/buildemain.java
+++ src/BuilderPatternPractice/buildemain.java
@@ -2,14 +2,15 @@ package BuilderPatternPractice;
 
 public class buildemain {
     public static void main(String[] args) {
-        builder b=new builder();
-        b.marks=100;
-        b.name="Abhi";
-        b.address="sahara";
-        b.psp=65.00;
-        b.University="Allahbad";
+        student s= student.getBuilder().setName("Hari")
+                .setUniversity("Michigan")
+                .setAddress("SRE")
+                .setMarks(100)
+                .build();
+        System.out.println(s);
 
-        student s= new student(b);
-        System.out.println(s.University);
+
+
+
     }
 }
--- src/BuilderPatternPractice/student.java
+++ src/BuilderPatternPractice/student.java
@@ -1,21 +1,84 @@
 package BuilderPatternPractice;
 
-public class student {
+public  class student {
     Integer marks;
     String name;
     String University;
     double psp;
     String address;
 
-    student(builder builder){
-        if(builder.marks<90){
-            System.out.println(" not in Topper");
-        }
+     public static builder getBuilder(){
+        return  new builder();
+    }
+
+    private student(builder builder){
+
         this.marks= builder.marks;
         this.address= builder.address;;
         this.name= builder.name;
         this.psp= builder.psp;
         this.University= builder.University;
+    }
+    public static class builder {
+        Integer marks;
+        String name;
+        String University;
+        double psp;
+        String address;
+
+        public String getName() {
+            return name;
+        }
+
+        public builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Integer getMarks() {
+            return marks;
+        }
+
+        public  builder setMarks(int marks) {
+            this.marks = marks;
+            return this;
+        }
+
+        public String getUniversity() {
+            return University;
+        }
+
+        public builder setUniversity(String university) {
+            University = university;
+            return this;
+        }
+
+        public double getPsp() {
+            return psp;
+        }
+
+        public  builder psp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public  builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public student build(){
+
+            if(this.marks<100){
+                System.out.println(" The marks are less");
+            }
+
+            return new student(this);
+        }
     }
 
 
